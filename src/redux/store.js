897f16e1679c3c619bd733a8c99271e00971be5c
Valentin787 <89661234+Valentin787@users.noreply.github.com>
2022-08-06 12:33:48 +0300

    final_commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -41,7 +41,7 @@ const store = configureStore({
       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
     }
   }).concat(logger),
-  devTools: process.env.NODE_ENV !== 'development',
+  devTools: process.env.NODE_ENV !== 'production',
 })
     
  const persistor = persistStore(store)
